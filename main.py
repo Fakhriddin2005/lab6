1ccbc542946b0679fdc2501d709bc06f2ebdb2a6
Fakhriddin2005 <160744102+Fakhriddin2005@users.noreply.github.com>
2024-04-11 00:06:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,56 @@
-def guessnum():
- import random
- random_numbers = [random.randint(1, 5) for _ in range(5)]
- user_number = int(input("Введите число: "))
- if user_number in random_numbers:
-    print("Поздравляю, Вы угадали число!")
- else:
-    print("Нет такого числа!")
- print("Исходный список:", random_numbers)
+def capitals():
+    dict_capitals={
+        'Франция': 'Париж', 'Германия': 'Берлин', 'Италия': 'Рим',
+        'Испания': 'Мадрид', 'Португалия': 'Лиссабон', 'Австрия': 'Вена'
+    }
+    country = input("Введите страну, чтобы узнать её столицу: ")
+    if country in dict_capitals:
+        print(f"Столица {country} - {dict_capitals[country]}")
+    else:
+        print("Такая страна не найдена в списке.")
+    sorted_countries = sorted(dict_capitals.keys())
+    print("\nСтраны в алфавитном порядке:")
+    for country in sorted_countries:
+        print(f"{country} - {dict_capitals[country]}")
+capitals()
 
-guessnum()
+def erudit():
+    score={
+        1 : ['А','В','Е','И','Н','О','Р','С','Т'],
+        2 : ['Д','К','Л','М','П','У'],
+        3 : ['Б','Г','Ё','Ь','Я'],
+        4 : ['Й','Ы'],
+        5 : ['Ж','З','Х','Ц','Ч'],
+        8 : ['Ш','Э','Ю',],
+        10 : ['Ф','Щ','Ъ',]
+    }
+    userword = input("Введите слово ")
+    userword=userword.upper()
+    d=0
+    for i in userword:
+        for k in score:
+            if i in score[k]:
+                d+=k
+    print('Ваши очки в игре эрудит: ' , d)
+erudit()
 
-def rep():
- import random
- list = [random.randint(1, 10) for _ in range(5)]
- duplicates = []
- for item in list:
-  if list.count(item)>1 and item not in duplicates:
-        duplicates.append(item)
- if duplicates:
-    print("Повторяющиеся элементы:", duplicates)
-    print (list)
- else:
-    print("Повторяющихся элементов нет")
-    print(list)
-rep()
-
-def holidays():
- days=('Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье', )
- restdays=int(input("Сколько дней отдыха вы хотите?"))
- weekend=days[-restdays:]
- workdays=days[:-restdays]
- print("Ваши выходные дни:",",".join(weekend))
- print("Ваши рабочие дни:",",".join(workdays))
-holidays()
-
-def groups():
- import random
- group1 = ['Панков', 'Молчанова', 'Абрамо', 'Колесников', 'Кондратьев', 'Тихонов', 'Лебедева', 'Родионов', 'Медведев', 'Романова']
- group2 = ['Воробьев', 'Пономарева', 'Морозов', 'Филатов', 'Золотарева', 'Давыдова', 'Матвеева', 'Попов', 'Коновалов', 'Павлов']
- print("Группа 1:", group1)
- print("Группа 2:", group2)
- team1 = random.sample(group1, 5)
- team2 = random.sample(group2, 5)
- sport_team = tuple(team1 + team2)
- print("Спортивная команда:", sport_team)
- print("Длина кортежа:", len(sport_team))
- sorted_sport_team = sorted(sport_team)
- print("Отсортированная команда по алфавиту:", sorted_sport_team)
- ivanov_count = sport_team.count("Иванов")
- if ivanov_count > 0:
-    print("Студент с фамилией 'Иванов' входит в команду. Количество вхождений:", ivanov_count)
- else:
-    print("Студент с фамилией 'Иванов' нету в команде.")
-groups()
+def lang():
+    students = {
+    "Alice": ["English", "Spanish", "French"],
+    "Bob": ["English", "Chinese"],
+    "Charlie": ["German", "Chinese", "Japanese"],
+    "David": ["English", "French"]
+}
+    languages = set()
+    for student, langlist in students.items():
+      for lang in langlist:
+        languages.add(lang)
+    sortedlanguages = sorted(list(languages))
+    print("Список различных языков, которые знают студенты:")
+    for lang in sortedlanguages:
+     print(lang)
+    print("\nСтуденты, которые знают китайский язык:")
+    for student, langlist in students.items():
+     if "Chinese" in langlist:
+        print(student)
+lang()
